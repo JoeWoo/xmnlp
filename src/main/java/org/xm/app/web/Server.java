6f3cc025707650532016a7c362eddb96cfafdae3
xuming <507153809@qq.com>
2016-05-26 20:02:51 +0800

    构建项目03

---
+++ src/main/java/org/xm/app/web/Server.java
@@ -0,0 +1,9 @@
+package org.xm.app.web;
+
+/**
+ * Created by xuming on 2016/5/26.
+ */
+public class Server {
+
+
+}
---
+++ src/main/java/org/xm/app/web/Servlet.java
@@ -0,0 +1,7 @@
+package org.xm.app.web;
+
+/**
+ * Created by xuming on 2016/5/26.
+ */
+public class Servlet {
+}
