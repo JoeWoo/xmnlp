6075b72bcc9704fabc2c928e4669f31a6c2d1e15
xuming <507153809@qq.com>
2016-05-26 20:05:03 +0800

    构建项目04

---
+++ src/test/java/Demo/AnalysisDemo.java
@@ -0,0 +1,16 @@
+package Demo;
+
+/**
+ *
+ * @Title: AnalysisDemo
+ * @Author: xuming
+ * @Description: 
+ * @date:2016/5/26 20:05
+ * @param 
+ * @return 
+ */
+public class AnalysisDemo {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
