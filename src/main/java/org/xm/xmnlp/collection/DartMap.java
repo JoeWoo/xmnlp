7df5c45bdd39d5251de591117bb8cf85b5dcedc2
mingzai <507153809@qq.com>
2016-07-24 17:18:20 +0800

    add 双数组trie树map，更省内存

--- src/main/java/org/xm/xmnlp/collection/DartMap.java
+++ src/main/java/org/xm/xmnlp/collection/DartMap.java
@@ -1,14 +1,3 @@
-/*
- * <summary></summary>
- * <author>He Han</author>
- * <email>hankcs.cn@gmail.com</email>
- * <create-date>2014/12/22 18:17</create-date>
- *
- * <copyright file="DartMap.java" company="上海林原信息科技有限公司">
- * Copyright (c) 2003-2014, 上海林原信息科技有限公司. All Right Reserved, http://www.linrunsoft.com/
- * This source is subject to the LinrunSpace License. Please contact 上海林原信息科技有限公司 to get more information.
- * </copyright>
- */
 package org.xm.xmnlp.collection;
 
 
@@ -20,7 +9,6 @@ import java.util.*;
 
 /**
  * 双数组trie树map，更省内存，原本希望代替DoubleArrayTrie，后来发现效率不够
- *
  */
 public class DartMap<V> extends DoubleArray implements Map<String, V>, ITrie<V> {
     V[] valueArray;
